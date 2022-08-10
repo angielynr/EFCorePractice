0e283af2cb3bd6015e15de11299cce4bde1cf2ba
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-08-10 14:14:05 +0800

    Update

---
+++ CalculatorConsoleApp/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            var res = a + b;
+            return res;
+        }
+    }
+}
---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculatorConsoleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            Console.WriteLine(calc.Add(1, 2));
+        }
+    }
+}
